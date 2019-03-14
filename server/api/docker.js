1e42f840eab91845a02b85f745b31241140bea19
Erina Kii <erina.kii92@gmail.com>
2019-03-14 13:00:34 -0400

    initial docker file

---
+++ server/api/docker.js
@@ -0,0 +1,15 @@
+// const router = require('express').Router()
+// const {NodeVM, VMScript} = require('vm2')
+
+// module.exports = router
+
+// router.get('/', async (req, res, next) => {
+//   try {
+//     const vm = new VM()
+//     if (typeof req.body === 'string') {
+//       const script = new VMScript(req.body)
+//       const output = vm.run(script)
+//       res.json(output)
+//     }
+//   } catch (error) {}
+// })
