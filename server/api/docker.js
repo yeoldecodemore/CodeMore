5513d626ebf52f0f511f20bf9fe816a567a140d5
Erina Kii <erina.kii92@gmail.com>
2019-03-15 15:04:12 -0400

    adding docker test

--- server/api/docker.js
+++ server/api/docker.js
@@ -1,15 +1,37 @@
-// const router = require('express').Router()
-// const {NodeVM, VMScript} = require('vm2')
+const router = require('express').Router()
+const {NodeVM} = require('vm2')
+module.exports = router
 
-// module.exports = router
+router.post('/', (req, res, next) => {
+  console.log(req.body.code)
+  res.send('okay!')
+})
 
 // router.get('/', async (req, res, next) => {
-//   try {
-//     const vm = new VM()
-//     if (typeof req.body === 'string') {
-//       const script = new VMScript(req.body)
-//       const output = vm.run(script)
-//       res.json(output)
-//     }
-//   } catch (error) {}
-// })
+// 	try {
+// 		const { code } = req.body;
+// 		const vm = new NodeVM({
+// 			require: {
+// 				timeout: 1000,
+// 				external: true,
+// 				mock: {
+// 					fs: {
+// 						readFileSync() {
+// 							return 'Nice try!';
+// 						}
+// 					}
+// 				}
+// 			}
+// 		});
+
+// 		const answerOne = vm.run(`${code} module.exports =isPalindrome("mom")`);
+// 		const answerTwo = vm.run(`${code} module.exports =isPalindrome("ew")`);
+// 		const obj = {
+// 			answerOne: answerOne,
+// 			answerTwo: answerTwo
+// 		};
+// 		res.json(obj);
+// 	} catch (error) {
+// 		next(error);
+// 	}
+// });
--- server/test/userinputtest.spec.js
+++ server/test/userinputtest.spec.js
@@ -1,27 +1,45 @@
-// const {expect} = require('chai')
+const {expect} = require('chai')
 
-// describe('isPalindrome', () => {
-//   it('is a function', () => {
-//     expect(isPalindrome).to.be.a('function')
-//   })
+const a = 'this is not a palindrome'
+const b = 'mom'
+const c = 0
 
-//   it('returns a boolean', () => {
-//     let returnedValue = isPalindrome('definitely not')
-//     expect(returnedValue).to.be.a('boolean')
-//   })
+function isPalindrome(string) {
+  if (string.length <= 1) {
+    return true
+  } else if (
+    string[0].toLowerCase() === string[string.length - 1].toLowerCase()
+  ) {
+    return isPalindrome(string.slice(1, string.length - 1))
+  } else {
+    return false
+  }
+}
 
-//   it('returns true if the passed string is a palindrome', () => {
-//     let returnedValue = isPalindrome('straw warts')
-//     expect(returnedValue).to.equal(true)
-//   })
+isPalindrome(a)
+isPalindrome(b)
+isPalindrome(c)
 
-//   it('returns false if the passed string is not palindrome', () => {
-//     let returnedValue = isPalindrome('this is not a palindrome')
-//     expect(returnedValue).to.equal(false)
-//   })
+it('is a function', () => {
+  expect(isPalindrome).to.be.a('function')
+})
 
-//   it('ignores differences in case', () => {
-//     let returnedValue = isPalindrome('UFOtofu')
-//     expect(returnedValue).to.equal(true)
-//   })
-// })
+it('returns a boolean', () => {
+  let returnedValue = isPalindrome('definitely not')
+  expect(returnedValue).to.be.a('boolean')
+})
+
+it('returns true if the passed string is a palindrome', () => {
+  let returnedValue = isPalindrome('straw warts')
+  expect(returnedValue).to.equal(true)
+})
+
+it('returns false if the passed string is not palindrome', () => {
+  let returnedValue = isPalindrome('this is not a palindrome')
+  expect(returnedValue).to.equal(false)
+})
+
+it('ignores differences in case', () => {
+  let returnedValue = isPalindrome('UFOtofu')
+  expect(returnedValue).to.equal(true)
+})
