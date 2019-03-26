cf7e2def0fd7aa5114ebc320fb0baa568b2290e6
Abbey Mondshein <5883999+abbeymondshein@users.noreply.github.com>
2019-03-26 17:17:12 -0400

    feat(login): update login interface to be more descriptive

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -6,6 +6,7 @@ export default () => (
   <div className="dragon">
     <a id="enterCodemore" href="/auth/github">
       <img src="https://bestanimations.com/Fantasy/Dragons/dragon-animated-gif-48-2.gif" />
+      <h6 className="speech-bubble">Login with Github</h6>
     </a>
   </div>
 )
