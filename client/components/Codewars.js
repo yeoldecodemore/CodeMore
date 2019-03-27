ef60608b35cde23c86acd0a97b9ea6de3aef0e90
Derek Harmanli <derek.harmanli@gmail.com>
2019-03-27 17:45:01 -0400

    new styling

--- client/components/Codewars.js
+++ client/components/Codewars.js
@@ -13,71 +13,74 @@ export default ({
   codewarsQuestions,
   generalCodewars,
   codewars
-}) => (
-  <div className="codewarsContainer">
-    <div className="codewarsTop">
-      <div className="codewarsCol codewarsColSize">
-        <div className="codewarsCol">
-          <h4 className="profileTitle" id="smaller">
-            Completed
-          </h4>
-          <div className="codewarssubtitle">
-            {generalCodewars.totalCompleted}
+}) => {
+  const CodewarsLogo = mapIcon('codewars')
+  return (
+    <div className="codewarsContainer">
+      <div className="codewarsTop">
+        <div className="codewarsCol codewarsColSize">
+          <div className="codewarsCol">
+            <h4 className="profileTitle" id="smaller">
+              Completed
+            </h4>
+            <div className="codewarssubtitle">
+              {generalCodewars.totalCompleted}
+            </div>
+          </div>
+          <div className="codewarsCol">
+            <h4 className="profileTitle" id="smaller">
+              Authored
+            </h4>
+            <div className="codewarssubtitle">
+              {generalCodewars.totalAuthored}
+            </div>
           </div>
         </div>
-        <div className="codewarsCol">
-          <h4 className="profileTitle" id="smaller">
-            Authored
-          </h4>
-          <div className="codewarssubtitle">
-            {generalCodewars.totalAuthored}
+        <div className="codewarsCol codewarsColSize">
+          <a
+            className="codewarsCol"
+            href={`https://www.codewars.com/users/${codewars}`}
+            target="_blank"
+          >
+            <div id="codewarsPic">
+              <CodewarsLogo className="codewarsLogo" />
+            </div>
+          </a>
+          <div className="codewarsCol">
+            <h4 className="profileTitle" id="smaller">
+              Most Recent Question
+            </h4>
+            <div className="codewarssubtitle">
+              {createDate(codewarsQuestions, 'questionCompletedAt')}
+            </div>
+          </div>
+        </div>
+        <div className="codewarsCol codewarsColSize">
+          <div className="codewarsCol">
+            <h4 className="profileTitle" id="smaller">
+              Leaderboard
+            </h4>
+            <div className="codewarssubtitle">
+              {generalCodewars.leaderboardPosition}
+            </div>
+          </div>
+          <div className="codewarsCol">
+            <h4 className="profileTitle" id="smaller">
+              Overall Rank
+            </h4>
+            <div className="codewarssubtitle">
+              {generalCodewars.overallRankName}
+            </div>
           </div>
         </div>
       </div>
-      <div className="codewarsCol codewarsColSize">
-        <a
-          className="codewarsCol"
-          href={`https://www.codewars.com/users/${codewars}`}
-          target="_blank"
-        >
-          <div id="codewarsPic">
-            <img src="./images/codewars.png" className="codewarsLogo" />
-          </div>
-        </a>
-        <div className="codewarsCol">
-          <h4 className="profileTitle" id="smaller">
-            Most Recent Question
-          </h4>
-          <div className="codewarssubtitle">
-            {createDate(codewarsQuestions, 'questionCompletedAt')}
-          </div>
-        </div>
-      </div>
-      <div className="codewarsCol codewarsColSize">
-        <div className="codewarsCol">
-          <h4 className="profileTitle" id="smaller">
-            Leaderboard
-          </h4>
-          <div className="codewarssubtitle">
-            {generalCodewars.leaderboardPosition}
-          </div>
-        </div>
-        <div className="codewarsCol">
-          <h4 className="profileTitle" id="smaller">
-            Overall Rank
-          </h4>
-          <div className="codewarssubtitle">
-            {generalCodewars.overallRankName}
-          </div>
+      <div className="codewarsBottom">
+        <div className="codewarslanguages">
+          {topCodewarsLanguagesViz(codewarsLanguages, 5).map((Logo, i) => (
+            <Logo key={i} />
+          ))}
         </div>
       </div>
     </div>
-    <div className="codewarsBottom">
-      <div className="codewarslanguages">
-        {topCodewarsLanguagesViz(codewarsLanguages, 5).map((Logo, i) => (
-          <Logo key={i} />
-        ))}
-      </div>
-    </div>
-  </div>
-)
+  )
+}
